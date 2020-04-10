68ec9a200d1b5414790f98ee716f728453fe8997
Vikas2004 <S538336@NWMISSOURI.EDU>
2020-04-10 15:22:08 -0500

    app.js commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'insert_your_database_url_here';
+var mongoDB = 'mongodb+srv://<vikas2004>:<vikas2004>@cluster0-mkp5q.azure.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
