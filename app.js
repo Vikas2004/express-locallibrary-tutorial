0d7d28abffc07159b60a38e4b4d9d92fa1a27132
Vikas2004 <S538336@NWMISSOURI.EDU>
2020-05-01 15:19:48 -0500

    final commit

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(helmet());
+app.use(helmet());  
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
